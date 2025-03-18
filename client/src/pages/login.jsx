6434272164cc0c6322273e7251ddb24d778a37af
Adrian Lezama <adrianlezama@Adrians-MBP.attlocal.net>
2025-03-18 16:53:06 -0400

    Runs concurrently

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -37,7 +37,7 @@ export default function Login() {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-16 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <div className="sm:mx-auto sm:w-full sm:max-w-ßsm">
           <img
             alt="Your Company"
             src={myLogo}
