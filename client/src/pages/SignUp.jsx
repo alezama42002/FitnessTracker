c047b587f99b0015ef3c83403dda4c4b169c9daf
Adrian Lezama <adrianlezama@Adrians-MBP.attlocal.net>
2025-03-19 18:50:46 -0400

    Sign Up page is almost completely done

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,42 @@
 import React from "react";
+import Input from "../components/Input";
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
 
 export default function SignUp() {
-  localStorage.removeItem("accessToken");
-  return <p>SignUp</p>;
+  return (
+    <div className="flex justify-center items-center h-screen bg-[#0E131F]">
+      <div className=" w-full mx-115 lg:mx-250">
+        <div className="flex flex-col justify-center items-center">
+          <img src={Logo} alt="" className="h-65" />
+          <h1 className="text-white font-normal text-[30px] pb-8 pt-3">
+            Create your Free Account
+          </h1>
+        </div>
+        <form
+          action="#"
+          method="POST"
+          className="space-y-6 p-8 rounded-[16px] bg-[#19212C]"
+        >
+          <Input inputName="Username:" />
+          <Input inputName="Password:" />
+          <Input inputName="Re-enter Password:" />
+          <div className="flex justify-center items-center">
+            <button className="bg-[#1B9E4B] rounded-[8px] px-14 mt-2 text-white font-normal text-[20px]">
+              Sign Up
+            </button>
+          </div>
+          <div className="flex justify-center items-center text-[14px] font-normal mt-6">
+            <p className="text-white pr-4 ">Already a member?</p>
+            <Link
+              to="/Auth/Login"
+              className="text-[#1B9E4B] text-[15px] font-normal"
+            >
+              Sign In
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -39,7 +39,7 @@ export default function Login() {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-16 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-ßsm">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             alt="Your Company"
             src={myLogo}
