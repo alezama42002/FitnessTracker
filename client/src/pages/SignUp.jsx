d7510d1ad365e7f5012eeab74b92d43257de67d9
Tommy <tommyngo156@gmail.com>
2025-03-19 23:12:24 -0400

    styled sign up page to be responsive to mobile devices

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -6,23 +6,23 @@ import Logo from "../assets/logo.png";
 export default function SignUp() {
   return (
     <div className="flex justify-center items-center h-screen bg-[#0E131F]">
-      <div className=" w-full mx-115 lg:mx-250">
+      <div className=" w-full mx-8 lg:mx-140">
         <div className="flex flex-col justify-center items-center">
-          <img src={Logo} alt="" className="h-65" />
-          <h1 className="text-white font-normal text-[30px] pb-8 pt-3">
+          <img src={Logo} alt="" className="h-30 lg:h-46" />
+          <h1 className="text-white font-normal text-[26px] lg:text-[30px] pb-8 pt-3">
             Create your Free Account
           </h1>
         </div>
         <form
           action="#"
           method="POST"
-          className="space-y-6 p-8 rounded-[16px] bg-[#19212C]"
+          className="space-y-6 p-8 rounded-[16px] bg-[#19212C] xs:p-4"
         >
           <Input inputName="Username:" />
           <Input inputName="Password:" />
           <Input inputName="Re-enter Password:" />
           <div className="flex justify-center items-center">
-            <button className="bg-[#1B9E4B] rounded-[8px] px-14 mt-2 text-white font-normal text-[20px]">
+            <button className="bg-[#1B9E4B] rounded-[8px] px-14 mt-2 text-white font-normal text-[18px]">
               Sign Up
             </button>
           </div>
