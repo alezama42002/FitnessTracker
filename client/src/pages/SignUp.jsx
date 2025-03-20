662f91ea44f865603962d42dd123abea8b291ec8
Tommy <tommyngo156@gmail.com>
2025-03-20 13:38:07 -0400

    ensure that sign up is responsive across multiple screen sizes

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -6,9 +6,9 @@ import Logo from "../assets/logo.png";
 export default function SignUp() {
   return (
     <div className="flex justify-center items-center h-screen bg-[#0E131F]">
-      <div className=" w-full mx-8 lg:mx-180">
+      <div className=" w-full mx-8 md:mx-40 lg:mx-60 xl:mx-130">
         <div className="flex flex-col justify-center items-center">
-          <img src={Logo} alt="" className="h-30 lg:h-46" />
+          <img src={Logo} alt="" className="h-30 md:h-40 lg:h-50" />
           <h1 className="text-white font-normal text-[26px] lg:text-[30px] pb-8 pt-3">
             Create your Free Account
           </h1>
