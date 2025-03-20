cc9b88f3c038056c8d303faeecf0c27787c77907
Adrian Lezama <143101600+alezama42002@users.noreply.github.com>
2025-03-19 23:59:59 -0400

    Update login.jsx

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,129 +1,69 @@
 import React from "react";
-import axios from "axios";
-import myLogo from "../assets/logo.png";
+import Logo from "../assets/Logo.png";
+import Input from "../components/Input";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
-export default function Login() {
+export default function AccountInformation() {
   const [formData, setFormData] = useState({
     Username: "",
     Password: "",
   });
-  const navigate = useNavigate();
 
-  const handleInputChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const login = async (event) => {
-    event.preventDefault();
-
-    try {
-      const response = await axios.post(
-        "http://localhost:3000/api/user/Login",
-        formData
-      );
-
-      if (response.data != "Not Allowed") {
-        localStorage.setItem("accessToken", response.data.accessToken);
-        navigate("/Dashboard/Progress");
-      }
-    } catch (error) {
-      return `Error: ${error}`;
-    }
+  const handleInputChange = (field, value) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [field]: value,
+    }));
   };
 
   return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-16 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="Your Company"
-            src={myLogo}
-            className="mx-auto h-30 w-auto"
+    <div className="bg-[#0E131F] flex justify-center items-center h-screen">
+      <div className=" w-full mx-115 lg:mx-90">
+        <form
+          action="#"
+          method="POST"
+          className="bg-[#19212C] space-y-6 rounded-[16px] p-8"
+        >
+          <div className="flex justify-center items-center">
+            <h1 className="text-white font-normal text-[30px]">
+              Account Information
+            </h1>
+          </div>
+          <Input
+            inputName="First Name:"
+            field="First-Name"
+            sendData={handleInputChange}
           />
-          <h2 className="mt-2 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
+          <Input
+            inputName="Last Name:"
+            field="Last-Name"
+            sendData={handleInputChange}
+          />
+          <Input
+            inputName="Gender:"
+            field="Gender"
+            sendData={handleInputChange}
+          />
+          <Input inputName="Age:" field="Age" sendData={handleInputChange} />
+          <Input
+            inputName="Weight:"
+            field="Weight"
+            sendData={handleInputChange}
+          />
+          <Input
+            inputName="Height:"
+            field="Height"
+            sendData={handleInputChange}
+          />
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form action="#" method="POST" className="space-y-6">
-            <div>
-              <label
-                htmlFor="username"
-                className="block text-sm/6 font-medium text-gray-900"
-              >
-                Username
-              </label>
-              <div className="mt-2">
-                <input
-                  id="Username"
-                  name="Username"
-                  type="text"
-                  onChange={handleInputChange}
-                  required
-                  autoComplete="Username"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm/6 font-medium text-gray-900"
-                >
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-[#0925D9] hover:text-[#5EA5FA]"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="Password"
-                  name="Password"
-                  type="Password"
-                  onChange={handleInputChange}
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-[#0966D9] px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-[#5EA5FA] focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={login}
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
-
-          <p className="mt-10 text-center text-sm/6 text-gray-500">
-            Not a member?{" "}
-            <a
-              href="#"
-              className="font-semibold text-[#0925D9] hover:text-[#5EA5FA]"
-            >
-              Create Account
-            </a>
-          </p>
-        </div>
+          <div className="flex justify-center items-center">
+            <button className="bg-[#1B9E4B] rounded-[8px] px-14 mt-2 text-white font-normal text-[20px]">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 }
+
