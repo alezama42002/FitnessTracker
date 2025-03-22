4061bc70bf4d88e21091e408043514d5455d43bf
Adrian Lezama <adrianlezama@Adrians-MacBook-Pro.local>
2025-03-21 21:10:42 -0400

    Dashboard almost complete aside from Micronutrients and still needs all logic for dynamicness of page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import DailyOverview from "../components/DailyOverview";
 import DailyFoods from "../components/DailyFoods";
+import MicroNutrients from "../components/MicroNutrients";
+import WeightGraph from "../components/LineChart";
 
 export default function Dashboard() {
   return (
     <div className="h-screen bg-[#0E131F] ">
       <Navbar />
-      <div className="mx-10 w-200">
-        <DailyOverview />
-        <DailyFoods />
+      <div className="grid grid-cols-3 gap-y-8 gap-x-8">
+        <div className="mx-10 w-200 col-span-2">
+          <DailyOverview />
+          <DailyFoods />
+        </div>
+
+        <div className="row-span-2 mt-8">
+          <MicroNutrients />
+        </div>
+        <div className="bg-[#19212C] h-150 ml-10 rounded-[16px] col-span-2 ">
+          <WeightGraph />
+        </div>
       </div>
     </div>
   );
