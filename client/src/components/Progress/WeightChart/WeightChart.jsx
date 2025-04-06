7ed312fc79505d93ec99ff845a525d6f7a4ac4c9
Tommy <tommyngo156@gmail.com>
2025-04-05 20:05:23 -0400

    added log weight popup for mobile screens

--- client/src/components/Progress/WeightChart/WeightChart.jsx
+++ client/src/components/Progress/WeightChart/WeightChart.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Chart from "react-apexcharts";
+import { useState } from "react";
+import PopUpWeight from "../../PopUpWeight";
 
 export default function WeightGraph({ weightData, weekDays }) {
+  const [open, setOpen] = useState(false);
+
   const chartConfig = {
     series: [
       {
@@ -70,10 +74,22 @@ export default function WeightGraph({ weightData, weekDays }) {
   return (
     <div className="relative flex flex-col rounded-xl bg-[#19212C] bg-clip-border text-gray-700 shadow-md h-full">
       <div className="relative mx-4 mt-4 flex flex-col gap-4 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none md:flex-row md:items-center">
-        <div>
+        <div className="relative flex justify-between items-center">
           <h1 className=" block font-sans text-base font-extrabold leading-relaxed tracking-normal text-white antialiased">
             Weight
           </h1>
+          <button
+            className="bg-[#1B9E4B] text-[12px] font-normal text-white px-4 py-2 rounded-[8px] cursor-pointer"
+            onClick={() => setOpen(true)}
+          >
+            Log Weight
+          </button>
+          {open && (
+            <PopUpWeight
+              onClose={() => setOpen(false)}
+              onSave={() => setOpen(false)}
+            />
+          )}
         </div>
       </div>
       <div className=" w-full p-2 flex-grow">
